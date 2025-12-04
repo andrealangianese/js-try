a95616cee3c1c28d33988f04b844a31581e000f2
andrealangianese <andre.langia.01@gmail.com>
2025-12-04 12:25:38 +0100

    feat: secondo manuale

--- script.js
+++ script.js
@@ -3,45 +3,64 @@
  L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
 */
 
-const primoNumero= 17;
-const secondoNumero = 20;
-const terzoNumero = 33;
+// const primoNumero= 17;
+// const secondoNumero = 20;
+// const terzoNumero = 33;
 
-if (primoNumero > secondoNumero) {
-    console.log(primoNumero)
-} else {
-    console.log(secondoNumero)
-}
+// if (primoNumero > secondoNumero) {
+//     console.log(primoNumero)
+// } else {
+//     console.log(secondoNumero)
+// }
 
-// opzione con 3 numeri e variabile
+// // opzione con 3 numeri e variabile
 
-let maggiore = primoNumero; 
-if (secondoNumero > maggiore) {
-    maggiore = secondoNumero;
-}
+// let maggiore = primoNumero; 
+// if (secondoNumero > maggiore) {
+//     maggiore = secondoNumero;
+// }
 
-if (terzoNumero > maggiore) {
-    maggiore = terzoNumero;
-}
+// if (terzoNumero > maggiore) {
+//     maggiore = terzoNumero;
+// }
 
-// col promt
+// // col promt
 
-let numberOne = prompt("scrivi due numeri");
+// let numberOne = prompt("scrivi due numeri");
 
-//per farmelo tornare un numero o potevo assegnare parseInt prima di promt
-let numeroUno = parseInt(numberOne);
+// //per farmelo tornare un numero o potevo assegnare parseInt prima di promt
+// let numeroUno = parseInt(numberOne);
 
-let numbertwo = prompt("scrivi il secondo numero");
-let numeroDue = parseInt(numbertwo);
+// let numbertwo = prompt("scrivi il secondo numero");
+// let numeroDue = parseInt(numbertwo);
 
-// se il numero uno è maggiore stampo il primo altrimenti stampo il secondo
+// // se il numero uno è maggiore stampo il primo altrimenti stampo il secondo
 
-if (numeroUno > numeroDue) {
-    console.log(numeroUno)
-}
-else {
-    console.log(numeroDue)
-}
+// if (numeroUno > numeroDue) {
+//     console.log(numeroUno)
+// }
+// else {
+//     console.log(numeroDue)
+// }
 
 // scritto uguale al primo ma col prompt e parseint
+
+
+// JSnack 3
+//  Il software deve chiedere per 5 volte all’utente di inserire un numero. 
+//  Il programma stampa la somma di tutti i numeri inseriti. 
+//  Esegui questo programma in due versioni, con il for e con il while (facoltativo)
+
+let dammiNumero1 = parseInt(prompt("scrivimi un numero"));
+let dammiNumero2 = parseInt(prompt("scrivimi un numero"));
+let dammiNumero3 = parseInt(prompt("scrivimi un numero"));
+let dammiNumero4 = parseInt(prompt("scrivimi un numero"));
+let dammiNumero5 = parseInt(prompt("scrivimi un numero"));
+
+console.log(dammiNumero1+dammiNumero2+dammiNumero3+dammiNumero4+dammiNumero5);
+
+// fatto a mano adesso devo provare a creare un ciclo
+
+
+
 
